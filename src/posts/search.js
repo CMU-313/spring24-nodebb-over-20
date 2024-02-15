60e18bd892d5167c88114e1022db2a44f5b51e39
Ben Nguyen Tran <hnguyentran03@gmail.com>
2024-02-15 18:56:19 -0500

    made non-functional code for searching

---
+++ src/posts/search.js
@@ -0,0 +1,54 @@
+
+
+const db = require('../database');
+
+module.exports = function (Posts) {
+    Posts.getPidsByContent = async function (content) {
+
+        // Presumably returns an array of pids
+        const pids = await db.scan({ match: `posts:pid:*` });    
+        const filteredPids = []
+
+        for (let i = 0; i < pids.length; i++) {
+            // From src/socket.io/helpers.js
+            const postData = await Posts.getPostFields(pids[i], ['content'])
+            
+            // Returns a postObj with a content field
+            const postObj = await Promise.all([
+                posts.parsePost(postData),
+            ]);
+            
+            // Manually uses string comparisons to check if the post has the included phrase 
+            if (postObj.content.includes(content)) {
+                filteredPids.push(pids[i])
+            }
+        }
+
+        return filteredPids
+
+    }
+}
+
+
+        // Approach 1:
+        
+        // Get all posts, using db.scan?
+
+        // const postData = await posts.getPostFields(pid, ['tid', 'uid', 'content']);
+
+        // const [userData, topicTitle, postObj] = await Promise.all([
+        //     user.getUserFields(fromuid, ['username']),
+        //     topics.getTopicField(postData.tid, 'title'), NOT NEEDED
+        //     posts.parsePost(postData),
+        // ]);
+
+        // postObj.content
+
+
+
+
+        // filter manually by looping and checking contents?
+
+        // Approach 2:
+        // filterAndSort?
+        // searchInContent?
