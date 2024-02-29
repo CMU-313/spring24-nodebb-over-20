9362a424252c5187635ccee78458aada678a8427
Levi Crosby <lcrosby@andrew.cmu.edu>
2024-02-29 18:20:19 -0500

    Fixed missing semicolon error

--- test/user.js
+++ test/user.js
@@ -96,7 +96,7 @@ describe('User', () => {
             assert.strictEqual(data.topiccount, 0);
             assert.strictEqual(data.lastposttime, 0);
             assert.strictEqual(data.accounttype, 'student');
-            assert.strictEqual(data.displayGroupTitle, 'Student')
+            assert.strictEqual(data.displayGroupTitle, 'Student');
             assert.strictEqual(data.banned, false);
         });
 
